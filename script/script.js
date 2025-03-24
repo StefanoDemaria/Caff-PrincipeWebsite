24711cfaad28b99fc8baac879d1251ca17eb9d40
stefano <stefano.demaria11@gmail.com>
2025-03-24 22:45:03 +0100

    terzo commit con aggiunta di Javascript

---
+++ script/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() {
+    const mainElement = document.querySelector('main');
+    mainElement.classList.add('loaded');
+});
+
+const changeH1color = () => {
+    let colorChanged; 
+    colorChanged = setTimeout
+};
+
+function
