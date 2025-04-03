f4136c18e3053722f13db66986455c4ace3e9f30
StefanoDemaria <stefano.demaria11@gmail.com>
2025-04-03 21:13:21 +0200

    seconda pagina in aggiornamneto

--- script/homepage.js
+++ script/homepage.js
@@ -41,6 +41,26 @@ function loadMoreContent () {
    ).then(data => {
     let div = document.createElement('div'); 
     div.classList.add('page'); 
-    
-   })
+    div.innerHTML = data;
+    document.getElementById('loading').appendChild(div); // Aggiungi il nuovo contenuto
+    page++;
+    loadiing = false; // Rimuovi il caricamento
+    document.getElementById('loading').style.display = 'none'; // Nascondi il caricamento
+
+   }).catch(error => {
+        console.error('Errore nel caricamento della pagina:', error);
+        loading = false; // Rimuovi il caricamento anche in caso di errore
+        document.getElementById('loading').style.display = 'none'; // Nascondi il caricamento
+   });
+   
+
 }; 
+
+window.addEventListener("scroll", () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
+        loadMoreContent();
+    }
+});
+
+// Carica la prima pagina inizialmente
+loadMoreContent();
